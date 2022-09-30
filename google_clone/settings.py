831e60cb3b43196db6bd98922b28dcf4b86f3ea4
ManeeshKrishna724 <91873467+ManeeshKrishna724@users.noreply.github.com>
2022-09-30 18:29:41 +0530

    p

--- google_clone/settings.py
+++ google_clone/settings.py
@@ -139,6 +139,6 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'pitbullbrowser724@gmail.com'
-EMAIL_HOST_PASSWORD = 'pitbull_7_2_4'
+EMAIL_HOST_PASSWORD = 'dqdqzmnpyspadudw'
 
 django_heroku.settings(locals())
